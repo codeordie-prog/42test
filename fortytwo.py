d0ca01e4a4610910a52d7c40c6d0938bdabaa5eb
Kelvin Ndeti <kelvinndeti3142@gmail.com>
2024-10-15 14:16:17 +0300

    nvd

--- fortytwo.py
+++ fortytwo.py
@@ -470,8 +470,8 @@ try:
                                  
                                 nim_response = ""
                                 for chunk in llm2.stream(input=user_input):
-                                      st.write(chunk.content,end="")
                                       nim_response+=chunk.content
+                                      st.write(nim_response)
 
                                 #st.session_state["messages"].append({"role": "assistant", "content": nim_response})
 
