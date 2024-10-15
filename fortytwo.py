e4129817c27c4129e4f6a3ab4e10865ab236943b
Kelvin Ndeti <kelvinndeti3142@gmail.com>
2024-10-15 13:58:13 +0300

    nvd

--- fortytwo.py
+++ fortytwo.py
@@ -470,8 +470,8 @@ try:
                             if api_provider=="openAI":
                                 response = llm_chain.run({"question": user_input}, callbacks = [stream_handler])
 
-                            elif api_provider=="NVIDIA":
-                                response = llm_chain.stream(input=user_input)
+                            
+                            response = llm_chain.stream(input=user_input)
 
 
                             #image generation function calling
